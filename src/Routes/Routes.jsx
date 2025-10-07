be35b39031fcf4771664a9a4e0304bbaafadd13f
Raihan <mdr8077@gmail.com>
2025-10-07 23:11:24 +0600

    Project initialize done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Components/Root/Root";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Root,
+        children: [
+            {index:true,Component: Home}
+        ]
+    }
+])
