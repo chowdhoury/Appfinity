c4fccf2e458268d9c1aea404d6ddd57c05af2741
Raihan <mdr8077@gmail.com>
2025-10-09 14:05:23 +0600

    Skeleton loader created

--- src/Hooks/useApps.js
+++ src/Hooks/useApps.js
@@ -1,28 +1,28 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const useApps = () => {
-    const [apps, setApps] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [apps, setApps] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        setLoading(true);
-        const startTime = Date.now();
-        
-        axios('/appsData.json')
-            .then(data => setApps(data.data))
-            .catch(err => setError(err))
-            .finally(() => {
-                const elapsedTime = Date.now() - startTime;
-                const remainingTime = Math.max(1000 - elapsedTime, 0);
-                
-                setTimeout(() => {
-                    setLoading(false);
-                }, remainingTime);
-            })
-    }, [])
-    return {apps,loading,error}
-}
+  useEffect(() => {
+    setLoading(true);
+    const startTime = Date.now();
+
+    axios("/appsData.json")
+      .then((data) => setApps(data.data))
+      .catch((err) => setError(err))
+      .finally(() => {
+        const elapsedTime = Date.now() - startTime;
+        const remainingTime = Math.max(1000 - elapsedTime, 0);
+
+        setTimeout(() => {
+          setLoading(false);
+        }, remainingTime);
+      });
+  }, []);
+  return { apps, loading, error };
+};
 
 export default useApps;
